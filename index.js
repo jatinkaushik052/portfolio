321bde8004beb23efe0311279c1fa96615594db4
JATIN KAUSHIK <149110777+jatinkaushik052@users.noreply.github.com>
2024-01-15 08:26:42 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function showSidebar(){
+        const sidebarm=document.querySelector('.sidebar')
+        sidebarm.style.display="flex"
+        console.log("hello")
+    }
+    function hideSidebar(){
+        const sidebarm=document.querySelector('.sidebar')
+        sidebarm.style.display="none"
+        console.log("hello")
+    }
+
+   
